a810e9faab68ff579d5a7b664729a1fff1f9a4d2
Timothee Cayrouse <timocayrouse@hotmail.fr>
2022-03-11 11:07:43 +0100

    ajout animation menu burger

---
+++ src/assets/js/app.js
@@ -0,0 +1,30 @@
+// les helpers
+function $(selector, el) {
+  if (!el) {
+    el = document;
+  }
+  return el.querySelector(selector);
+}
+
+function $$(selector, el) {
+  if (!el) {
+    el = document;
+  }
+  return Array.prototype.slice.call(el.querySelectorAll(selector));
+}
+
+
+window.addEventListener('DOMContentLoaded', function () {
+  
+  $('.header__navBar svg').addEventListener('click', function(){
+    $('#nav__links').classList.remove('header__navClose');
+    $('#nav__links').classList.add('header__navOpen');
+  });
+
+  $('#nav__links svg').addEventListener('click', function(){
+    $('#nav__links').classList.add('header__navClose');
+    $('#nav__links').classList.remove('header__navOpen');
+  });
+
+
+});
