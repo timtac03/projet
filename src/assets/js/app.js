ba5fa1d720b3b153062a15b2918643a4188160e5
Timothee Cayrouse <timocayrouse@hotmail.fr>
2022-03-14 22:03:35 +0100

    ajout d'effet soulignement aux h2 et des images triforce aux h1

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -23,4 +23,19 @@ window.addEventListener("DOMContentLoaded", function () {
     $("#nav__links").classList.add("header__navLinks--Close");
     $("#nav__links").classList.remove("header__navLinks--Open");
   });
+
+  window.addEventListener("scroll", function () {
+    $$(".main__articleUnderline").forEach((h2) => {
+      let targetTop = h2.offsetTop;
+      windowHeight = document.documentElement.clientHeight;
+      if (window.pageYOffset + windowHeight >= targetTop) {
+        h2.classList.add("main__articleUnderline2");
+        if (screen.width >= 700) {
+          $(".main__articleTablet > h2").classList.remove(
+            "main__articleUnderline--center"
+          );
+        }
+      }
+    });
+  });
 });
